d633712d42e7be205551228f762be3da7757618d
SashaTatu <24b_tod@liceum.ztu.edu.ua>
2025-03-03 12:46:13 +0200

    Змінений розділ з порадами

--- js/script.js
+++ js/script.js
@@ -1,74 +1,75 @@
 console.log('Script connected');
 
-const arrayOfLoveWishes = [
-    "Бажаю тобі нескінченної любові та тепла!",
-    "Нехай твоє серце завжди буде сповнене ніжності.",
-    "Нехай кожен день приносить радість і кохання.",
-    "Бажаю, щоб поруч завжди була людина, яка тебе кохає.",
-    "Нехай кохання буде чистим, щирим і взаємним.",
-    "Нехай у твоєму житті буде багато обіймів і поцілунків.",
-    "Бажаю, щоб твоє серце ніколи не знало болю і смутку.",
-    "Нехай кохання наповнює кожен твій день яскравими емоціями.",
-    "Бажаю вірного і турботливого партнера поруч.",
-    "Нехай твоє серце завжди знаходить відгук у іншому серці.",
-    "Бажаю, щоб ваше кохання було вічним і міцним.",
-    "Нехай твоє життя буде наповнене щасливими моментами з коханою людиною.",
-    "Бажаю пристрасних і незабутніх поцілунків.",
-    "Нехай твоя любов буде натхненням для нових звершень.",
-    "Бажаю романтики і казкових моментів у стосунках.",
-    "Нехай ваші серця б’ються в унісон назавжди.",
-    "Бажаю, щоб ваше кохання було міцним, як діамант.",
-    "Нехай кожен день приносить вам нові причини для щастя разом.",
-    "Бажаю багато тепла, розуміння і гармонії у відносинах.",
-    "Нехай ваше кохання буде яскравим, як найкрасивіший схід сонця!"
-  ];
-  let countOfHearts = 5
-console.log('countOfHearts ' + countOfHearts)
+const arrayOfLoveadvice = [
+    'Бажаю тобі нескінченної любові та тепла!',
+    'Нехай твоє серце завжди буде сповнене ніжності.',
+    'Нехай кожен день приносить радість і кохання.',
+    'Бажаю, щоб поруч завжди була людина, яка тебе кохає.',
+    'Нехай кохання буде чистим, щирим і взаємним.',
+    'Нехай у твоєму житті буде багато обіймів і поцілунків.',
+    'Бажаю, щоб твоє серце ніколи не знало болю і смутку.',
+    'Нехай кохання наповнює кожен твій день яскравими емоціями.',
+    'Бажаю вірного і турботливого партнера поруч.',
+    'Нехай твоє серце завжди знаходить відгук у іншому серці.',
+    'Бажаю, щоб ваше кохання було вічним і міцним.',
+    'Нехай твоє життя буде наповнене щасливими моментами з коханою людиною.',
+    'Бажаю пристрасних і незабутніх поцілунків.',
+    'Нехай твоя любов буде натхненням для нових звершень.',
+    'Бажаю романтики і казкових моментів у стосунках.',
+    'Нехай ваші серця б’ються в унісон назавжди.',
+    'Бажаю, щоб ваше кохання було міцним, як діамант.',
+    'Нехай кожен день приносить вам нові причини для щастя разом.',
+    'Бажаю багато тепла, розуміння і гармонії у відносинах.',
+    'Нехай ваше кохання буде яскравим, як найкрасивіший схід сонця!',
+];
+let countOfHearts = 5;
+console.log('countOfHearts ' + countOfHearts);
 
+document.getElementById('count-of-pills').innerText = '🧡'.repeat(
+    countOfHearts
+);
 
-document.getElementById('count-of-hearts').innerText = '🧡'.repeat(countOfHearts)
+document.getElementById('btn_advice').addEventListener('click', () => {
+    console.log('Button clicked');
 
+    let index = Math.floor(Math.random() * arrayOfLoveadvice.length);
 
-document.getElementById('btn_wishes').addEventListener('click', () =>  {
-  console.log('Button clicked');
+    //console.log('Номер елементу масиву - ', index);
+    document.getElementById('p-advice').innerText = arrayOfLoveadvice[index];
 
+    countOfHearts--;
+    console.log('countOfHearts ' + countOfHearts);
+    document.getElementById('count-of-pills').innerText =
+        '🧡'.repeat(countOfHearts) + '🤍'.repeat(5 - countOfHearts);
 
-  let index = Math.floor(Math.random() * arrayOfLoveWishes.length)
-   
-  //console.log('Номер елементу масиву - ', index);
-  document.getElementById('p_wishes').innerText = arrayOfLoveWishes[index];
+    let randomIMG = Math.floor(Math.random() * 13) + 1;
+    console.log('randomIMG ' + randomIMG);
+    document.getElementById('medical-img').src = `img/gallery/${randomIMG}.png`;
+    document.getElementById('medical-img').alt = 'Heart for U';
 
+    document.getElementById('medical-img').style.display = 'flex';
 
-  countOfHearts--
-  console.log('countOfHearts ' + countOfHearts)
-  document.getElementById('count-of-hearts').innerText = '🧡'.repeat(countOfHearts) + '🤍'.repeat(5-countOfHearts)
+    if (countOfHearts == 0) {
+        document.getElementById('btn_advice').style.display = 'none';
+        document.getElementById('count-of-pills').innerText =
+            ' Придбайте нові сердечка🧡';
+    }
+});
 
+document.getElementById('count-of-pills').addEventListener('click', () => {
+    if (countOfHearts == 0) {
+        alert('Придбати сердечка можна натиснувши кнопку "Купити🧡"');
+    }
+});
 
-  let randomIMG = Math.floor(Math.random()*13)+1
-  console.log('randomIMG '+ randomIMG);
-  document.getElementById('love-img').src = `img/gallery/${randomIMG}.png`;
-  document.getElementById('love-img').alt = "Heart for U";
+document.getElementById('btn-buy-pills').addEventListener('click', () => {
+    countOfHearts = 5;
+    document.getElementById('count-of-pills').innerText = '🧡'.repeat(
+        countOfHearts
+    );
+    document.getElementById('btn_advice').style.display = 'inline-block';
+    document.getElementById('p-advice').innerText = '';
+    document.getElementById('medical-img').style.display = 'none';
 
-  document.getElementById('love-img').style.display = 'flex'
-
-  if(countOfHearts == 0) {
-    document.getElementById('btn_wishes').style.display = 'none'
-    document.getElementById('count-of-hearts').innerText = ' Придбайте нові сердечка🧡'
-  }
-})
-  
-document.getElementById('count-of-hearts').addEventListener('click', () =>{
-  if(countOfHearts == 0) {
-    alert('Придбати сердечка можна натиснувши кнопку "Купити🧡"')
-  }
-})
-
-document.getElementById('btn-buy-hearts').addEventListener('click', () =>  {
-  countOfHearts = 5
-  document.getElementById('count-of-hearts').innerText = '🧡'.repeat(countOfHearts)
-  document.getElementById('btn_wishes').style.display = 'inline-block'
-  document.getElementById('p_wishes').innerText = ''
-  document.getElementById('love-img').style.display = 'none'
-
-  console.log('btn_buy-hearts clicked')
-})
+    console.log('btn_buy-hearts clicked');
+});
