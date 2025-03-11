42f32599d119d649cd6dd3b8d162650a50513324
SashaTatu <24b_tod@liceum.ztu.edu.ua>
2025-03-12 00:44:14 +0200

    видалено зайве

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ const arrayOfAdvice = [
 let countOfPills = 5;
 console.log('countOfPills ' + countOfPills);
 
-let showAlert = 3;
+let showAlert = 2;
 if (showAlert) {
     alert('Якщо що калькулятор я видалю');
     showAlert--;
@@ -83,16 +83,8 @@ document.getElementById('btn-buy-pills').addEventListener('click', () => {
 });
 
 
-let press = 0
-
 document.getElementById('btn_calculate').addEventListener('click', () => {
     console.log('Calculate clicked');
-
-    press++
-    console.log('Person press button '+press)
-    if (press==3){
-        alert('Поставите хорошу оцінку?)')
-    }
 
     let weight = parseFloat(document.getElementById('weight').value);
     console.log('weight ' + weight);
